19471454df670cd921ad943f1abc8a91f00638d8
Abayomi <eyitouyoa3@gmail.com>
2022-02-09 13:36:13 -0500

    created activityFeed action creator

---
+++ src/store/actions/activity-feed-actions.js
@@ -0,0 +1,29 @@
+import { activityFeedActions } from "../slices/activity-feed-slice";
+
+const baseUrl = "https://aircall-job.herokuapp.com";
+
+export const getCallData = () => {
+    return async (dispatch) => {
+        const fetchCalls = async () => {
+            const response = await fetch(`${baseUrl}/activities`);
+
+            if (!response.ok) {
+                throw new Error('Could not fetch the calls :(');
+            }
+
+            const data = await response.json();
+
+            return data;
+        }
+
+        try {
+            const calls = await fetchCalls()
+
+            dispatch(activityFeedActions.getCalls(calls))
+        }
+        catch (error) {
+            console.log(error);
+        }
+
+    }
+}
