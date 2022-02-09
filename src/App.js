674385a78097c393b9b75bcac17fb026f8168ed5
Abayomi <eyitouyoa3@gmail.com>
2022-02-09 13:37:34 -0500

    can fetch call data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
 
 import styles from './index.module.css';
 import Header from './Header';
 
+import store from './store/store';
+import { Provider } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { getCallData } from './store/actions/activity-feed-actions';
+
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getCallData());
+  }, [dispatch])
+
   return (
     <div className={styles.app}>
       <div className={styles.container}>
@@ -15,6 +26,6 @@ const App = () => {
   );
 };
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('app'));
 
 export default App;
